656837f33edcb9c1950e132c41147127242605cb
cc <ccdongdl@cn.ibm.com>
2020-05-03 16:08:43 +0800

    2020-05-03 date select add to sellerreporter

--- eMartAngular/src/app/components/sellreport/sellreport.component.ts
+++ eMartAngular/src/app/components/sellreport/sellreport.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { PurchaseService } from '../../services/purchase.service';
-// import { ItemDetailComponent } from '../item-detail/item-detail.component';
+
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sellreport',
@@ -13,13 +14,14 @@ export class SellreportComponent implements OnInit {
   constructor(private purchaseService:PurchaseService ) { }
 
   ngOnInit(): void {
-      this.sellReportList = this.getReportData();
+      this.purchaseListById = this.purchaseService.getPurchaseListBySellerId(localStorage.getItem('email'));
+      this.sellReportList = this.getReportData(this.purchaseListById);
   }
 
   sellReportList:any[] = [];
+  purchaseListById:any[] = [];
 
-  getReportData(){
-    let purchaseList = this.purchaseService.getPurchaseListBySellerId(localStorage.getItem('email'));
+  getReportData(purchaseList:any[]){
     let reportList:any[] = [];
     let reportdata:any;
     for(let i  = 0; i < purchaseList.length; i++){
@@ -33,9 +35,10 @@ export class SellreportComponent implements OnInit {
                 reportList.push(reportdata);
                 reportdata = oldData;
             }
-            if(i === purchaseList.length - 1){
-                reportList.push(reportdata);
-            }
+            
+        }
+        if(i === purchaseList.length - 1){
+            reportList.push(reportdata);
         }
 
     }
@@ -43,6 +46,28 @@ export class SellreportComponent implements OnInit {
     
   }
 
+  startDate:NgbDateStruct;
+  endDate:NgbDateStruct;
+  onSubmit(){
+    let listbyDate:any[] = [];
+    // console.log(this.startDate);
+    // console.log(this.endDate);
+    if(this.startDate && this.endDate){
+        this.purchaseService.purchaseSampleData.forEach(item => {
+            if (new Date(item.dateTime) >= new Date(this.startDate.year,this.startDate.month - 1,this.startDate.day) && 
+            new Date(item.dateTime) <= new Date(this.endDate.year,this.endDate.month - 1,this.endDate.day)){
+                console.log(item);
+                listbyDate.push(item);
+                //.replace(/-/g, "/")
+            }
+        });
+        console.log(listbyDate);
+        this.sellReportList = this.getReportData(listbyDate);
+    }
+    
+    
+    
+  }
 
 
 }
--- eMartAngular/src/app/services/purchase.service.ts
+++ eMartAngular/src/app/services/purchase.service.ts
@@ -7,10 +7,10 @@ export class PurchaseService {
 
   constructor() { }
   purchaseSampleData = [
-      {id:1, buyerId:"tester@test.com", sellerId:"seller@test.com", transferId:1, itemId:1, itemName:"xxx", tax:0.2, itemNumbers:4, itemPrice: 7000, dateTime:"2019-05-06", discount:0.2},
-      {id:2, buyerId:"tester@test.com", sellerId:"seller@test.com", transferId:2, itemId:3, itemName:"yyy", tax:0.2, itemNumbers:1, itemPrice: 500, dateTime:"2019-04-08", discount:0},
-      {id:3, buyerId:"buyer1@test.com", sellerId:"seller@test.com", transferId:5, itemId:3, itemName:"yyy", tax:0.2, itemNumbers:5, itemPrice: 500, dateTime:"2019-06-10", discount:0},
-      {id:4, buyerId:"buyer@test.com", sellerId:"seller1@test,com", transferId:7, itemId:2, itemName:"zzz", tax:0.2, itemNumbers:1, itemPrice: 8000, dateTime:"2019-06-18", discount:0}
+      {id:1, buyerId:"tester@test.com", sellerId:"seller@test.com", transferId:1, itemId:1, itemName:"xxx", tax:0.2, itemNumbers:4, itemPrice: 7000, dateTime:"2020-05-06", discount:0.2},
+      {id:2, buyerId:"tester@test.com", sellerId:"seller@test.com", transferId:2, itemId:3, itemName:"yyy", tax:0.2, itemNumbers:1, itemPrice: 500, dateTime:"2020-04-08", discount:0},
+      {id:3, buyerId:"buyer1@test.com", sellerId:"seller@test.com", transferId:5, itemId:3, itemName:"yyy", tax:0.2, itemNumbers:5, itemPrice: 500, dateTime:"2020-03-10", discount:0},
+      {id:4, buyerId:"buyer@test.com", sellerId:"seller1@test,com", transferId:7, itemId:2, itemName:"zzz", tax:0.2, itemNumbers:1, itemPrice: 8000, dateTime:"2020-05-18", discount:0}
   ]
   getPurchaseHistoryByBuyerId(buyerid:string){
     // get from backend.
